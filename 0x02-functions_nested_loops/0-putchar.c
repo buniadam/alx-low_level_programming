c3b402503c83c62426b1205403058626880a2649
buniadam <buniadam24@gmail.com>
2022-09-13 15:01:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-        _putchar('_');
+	_putchar('_');
         _putchar('p');
         _putchar('u');
         _putchar('t');
@@ -16,7 +16,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
+	
 	return (0);
 
 }
