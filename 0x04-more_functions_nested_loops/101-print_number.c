5fe98ff9b856f5ea1a0e84121ba88c4f32714554
buniadam <buniadam24@gmail.com>
2022-09-15 13:56:12 -0700

    my task correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * print_number - Prints number
- * @n: The number to print
- */
+*print_number - Prints number
+*@n: The number to print
+*/
 
 void print_number(int n)
 {
