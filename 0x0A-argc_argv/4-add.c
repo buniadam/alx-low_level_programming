5697877b83cacc0b7119013185a705e08cdbbf9f
buniadam <buniadam24@gmail.com>
2022-09-30 06:22:13 -0700

    my task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define UNUSED(x) (void)(x)
 
 /**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
+ * main - add
+ * @argc:int
+ * @argv:string
+ * Return:int
  */
-int StringCheck(char *s)
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int x = 0, i, j;
 
-	for (; s[i] != '\0'; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(s[i]))
+		for (j = 0; argv[i][j]; j++)
 		{
-			return (0);
-		}
-	}
-	return (1);
-}
-/**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- * Return: always 0
- */
-int main(int argc, char  *argv[])
-{
-	int i;
-	int result = 0;
-
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
+			if (isdigit(argv[i][j]) == 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		printf("%d\n", result);
-		return (0);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		printf("%d\n", 0);
-		return (1);
+		x += atoi(argv[i]);
 	}
-
+	printf("%d\n", x);
+	return (0)
 }
