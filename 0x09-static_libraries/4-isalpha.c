92f06d7f5bc943b2985ef792f5e58de2e6048712
buniadam <buniadam24@gmail.com>
2022-10-01 01:05:00 -0700

    task 0

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -4,13 +4,12 @@
  * @c: an integer argument
  * Return: 0
  */
-
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
-return (1);
-}
-else
-return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
