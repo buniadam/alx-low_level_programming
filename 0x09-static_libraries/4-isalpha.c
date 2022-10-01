73dbeb70a0443c07a939a7c39035ee556c1725d6
buniadam <buniadam24@gmail.com>
2022-10-01 00:57:58 -0700

    task 0

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _isalpha - its a function in the main header that allows us to print
- * @c: is the int that will use for the argument of the function
+ * @c: an integer argument
  * Return: 0
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-		return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+return (0);
 }
