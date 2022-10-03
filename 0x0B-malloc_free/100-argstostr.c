8298ed8543d546017811e3b04801f134c40b2dd0
buniadam <buniadam24@gmail.com>
2022-10-03 09:23:10 -0700

    my task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,85 +1,39 @@
+
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * helper - helps function
- * @word: wordcount
- * @len: length
- * @str: string to go through
- * @s: array you are assigning
- * Return: char value
+ * **argstostr -  a function that concatenates all the arguments of your program.
+ * *@ac : int
+ * *@av : array
+ * *Return: array
  */
-char **helper(int word, int len, char *str, char **s)
+char *argstostr(int ac, char **av)
 {
-	int i, k, j;
+	int x, y, i, j, l = 0, a = 0;
+	char *s;
 
-	j = 0;
-	for (i = 0; i < word; i++)
+	if (ac == 0 || av == NULL)
+	return (NULL);
+	for (i = 0; (i < ac); i++)
 	{
-		k = 0;
-		for (; j < len; j++)
-		{
-			if (str[0] != ' ' || str[j] != ' ')
-			{
-				s[i][k] = str[j];
-				k++;
-			}
-			if (j != 0 && str[j] == ' ' && str[j - 1] != ' ')
-			{
-				j++;
-				break;
-			}
-		}
-		s[i][k + 1] = '\0';
+		for (j = 0; av[i][j] != '\0'; j++)
+		l++;
+		l++;
 	}
-	s[word + 1] = NULL;
-	return (s);
-}
-/**
- * strtow - string to words
- * @str: string to check
- * Return: return char value
- */
-char **strtow(char *str)
-{
-	int len, i, j, size, k, word;
-	char **s;
 
-	if (str == NULL)
-		return (NULL);
-	len = 0;
-	word = 0;
-	while (str[len] != '\0')
-	{
-		if (str[0] != ' ')
-		word++;
-		if (str[len] != ' ' && str[len - 1] == ' ' && len != 0)
-			word++;
-		len++;
-	}
-	s = (char **)malloc(sizeof(char *) * word + 1);
+	s = malloc(sizeof(char) * l + 1);
 	if (s == NULL)
-		return (NULL);
-	j = 0;
-	for (i = 0; i < word; i++)
+	return (NULL);
+	for (x = 0; x < ac ; x++)
 	{
-		size = 0;
-		for (; j < len; j++)
+		for (y = 0; av[x][y] != '\0'; y++)
 		{
-			if (str[0] != ' ' || str[j] != ' ')
-				size++;
-			if (str[j] == ' ' && size > 0)
-				break;
+			s[a] = av[x][y];
+			a++;
 		}
-		printf("%d\n", size);
-		s[i] = (char *)malloc(sizeof(char) * size + 1);
-		if (s[i] == NULL)
-		{
-			for (k = i - 1; k >= 0; k--)
-				free(s[k]);
-			free(s);
-		}
+		s[a++] = '\n';
 	}
-	s = helper(word, len, str, s);
+	s[a] = '\0';
 	return (s);
 }
