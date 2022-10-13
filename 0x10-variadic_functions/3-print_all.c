c971da745d36ee3dddd74edb1125c9f3806c8ecd
buniadam <buniadam24@gmail.com>
2022-10-13 15:02:59 -0700

    my task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
-	const char t_arg[] = "tifs";
+	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
 	while (format && format[i])
