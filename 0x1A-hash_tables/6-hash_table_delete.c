9e69304bfb3338c0527bb664ee13c2cefba15e37
buniadam <buniadam24@gmail.com>
2023-01-06 12:10:56 -0800

    task

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
  */
-void hash_table_delete(hash_table_t *ht)
+int main(void)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
+	hash_table_t *ht;
+	char *key;
+	char *value;
 
-	for (i = 0; i < ht->size; i++)
-	{
-		if (ht->array[i] != NULL)
-		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
-		}
-	}
-	free(head->array);
-	free(head);
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "c", "fun");
+	hash_table_set(ht, "python", "awesome");
+	hash_table_set(ht, "Jennie", "and Jay love asm");
+	hash_table_set(ht, "N", "queens");
+	hash_table_set(ht, "Asterix", "Obelix");
+	hash_table_set(ht, "Betty", "Holberton");
+	hash_table_set(ht, "98", "Battery Streetz");
+	key = strdup("Tim");
+	value = strdup("Britton");
+	hash_table_set(ht, key, value);
+	key[0] = '\0';
+	value[0] = '\0';
+	free(key);
+	free(value);
+	hash_table_set(ht, "98", "Battery Street");
+	hash_table_set(ht, "hetairas", "Jennie");
+	hash_table_set(ht, "hetairas", "Jennie Z");
+	hash_table_set(ht, "mentioner", "Jennie");
+	hash_table_set(ht, "hetairas", "Jennie Z Chu");
+	hash_table_print(ht);
+	hash_table_delete(ht);
+	return (EXIT_SUCCESS);
 }
